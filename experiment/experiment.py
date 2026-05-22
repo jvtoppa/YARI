f179bfd60eedc8f0a45335014fa57869dff9543f
jvtoppa <jvtoppa@gmail.com>
2026-05-22 00:07:48 -0300

    f: fixed experiments

--- experiment/experiment.py
+++ experiment/experiment.py
@@ -1,5 +1,7 @@
 import subprocess
 import os
+from pathlib import Path
+import re
 
 def peak_memory(massif_file):
     if not os.path.exists(massif_file):
@@ -21,7 +23,7 @@ def experiment():
     base_report_dir = 'memory_reports'
     
     with open("experiments.csv", "w") as f:
-        f.write("Filename, Peak Memory (MiB), Compression time (ms), Data Type\n")
+        f.write("Filename, Peak Memory (MiB), Compression time (ms), Encoding time (ms), Bitsize, No. of rules, CFG size, Data Type\n")
         
         for root, dirs, files in os.walk(base_dataset_dir):
             for filename in files:
@@ -48,12 +50,41 @@ def experiment():
                 except Exception as e:
                     peak_mib = f"ERROR_{type(e).__name__}"
 
+                time_result = subprocess.run(f"../build/repair < {dataset_path}",capture_output=True,shell=True,text=True)
+                comp_time = "FAILED"
+                rule_history_size = None
+                compressed_size = None
 
-                print(f"Profiling Time: {filename}")
-                time_result = subprocess.run(f"../build/repair < {dataset_path}", capture_output=True, shell=True, text=True)
-                comp_time = time_result.stdout.strip() if time_result.returncode == 0 else "FAILED"
+                if time_result.returncode == 0:
+                    output = time_result.stdout.strip()
 
-                log_line = f"{filename}, {peak_mib}, {comp_time}, {category}"
+                    rule_match = re.search(r"Rule History size:\s*(\d+)", output)
+                    size_match = re.search(r"Compressed text size:\s*(\d+)", output)
+                    time_match = re.search(r"([\d.]+)\s*ms", output)
+
+                    if rule_match:
+                        rule_history_size = int(rule_match.group(1))
+
+                    if size_match:
+                        compressed_size = int(size_match.group(1))
+
+                    if time_match:
+                        comp_time = f"{time_match.group(1)} ms"
+
+                print(f"Profiling bitsize: {filename}: ")
+                bitsize_result = subprocess.run(f"../build/encodeCFG", capture_output=True, shell=True, text=True)
+                file_path = Path(f"../output/compressed_cfg.bin")
+                time_match_bitsize = None
+                bitsize_time = "FAILED"
+                if bitsize_result.returncode == 0:
+                    output = bitsize_result.stdout.strip()
+                    print("enters")
+                    time_match_bitsize = re.search(r"Time taken:\s*([\d.]+)", output)
+                    if time_match_bitsize:
+                        bitsize_time = float(time_match_bitsize.group(1))
+                bitsize = file_path.stat().st_size * 8
+                
+                log_line = f"{filename}, {peak_mib}, {comp_time}, {bitsize_time}, {bitsize}, {int(rule_history_size / 2)}, {compressed_size}, {category}"
                 f.write(log_line + "\n")
                 f.flush()
                 print(f"Result: {log_line}\n")
